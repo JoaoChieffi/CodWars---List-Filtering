936069fd8bb5bc069ad566e2e428c781f04676f7
João Lucas Chieffi <40673059+chieffixgo@users.noreply.github.com>
2019-08-01 19:49:09 -0300

    Taka CodeWars. Feito em 01.08.2019

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace List_Filtering
+{
+    class Program
+    {    
+         static void Main(string[] args)
+        {
+            GetIntegersFromList(new List<object>(){1,2,"aasf","1","123",123});
+        }
+
+       public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+       {
+              listOfItems.RemoveAll(RemoveAllStrings);
+
+              return listOfItems.Select(s => Convert.ToInt32(s)).ToList();
+        } 
+
+        public static bool RemoveAllStrings(object value)
+        {
+            return (value is string);
+        }
+
+
+    }
+}
